f91c0e43a38055572509c9c7acd294128bccb440
2344pritam <mca.2344@unigoa.ac.in>
2023-10-14 17:16:09 +0530

    added infix and postfixevaluation

--- main.c
+++ main.c
@@ -1,20 +1,89 @@
-#include<stdio.h>
-#include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdbool.h>
+#include "./stack/stack.h"
+#include "./infix_to_postfix/infix_to_postfix.h"
+#include "./postfix_evaluation/postfix_evaluation.h"
+
 int main()
-{  int n;
-st s1;
-    do
-    {  
-       	printf("\nEnter 1:for merge sort \nEnter 2:for add \nEnter 3:for insertion sort \nEnter 4:for postfix evaluation \nEnter 0:for exit\n");
-   	scanf("%d",&n);
-   	
-   	switch(n)
-    {case 1:
-    createStack(&s1);
-    push(&s1,23);
-    printf("%d",pop(&s1));
-    }
-        
-    } while (n);
-    
-}
+{
+    int option;
+    int size;
+    while (1)
+    {
+        printf("Enter:\n1)Stack \n2)Infix to Postix \n3)Postfix Evalution \n4)Bracket Matching \n5)Binary Tree \n6)Shortest Distance \n7)Qiuck Sort \n8) Merge Sort");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+        {
+            stack *s = createStack();
+            int element;
+            do
+            {
+                printf("1)Push \n2)Pop \n3)Peek \n4)Print stack \n5)Exit to exit\n");
+                scanf("%d", &option);
+                switch (option)
+                {
+                case 1:
+                {
+                    fflush(stdin);
+                    printf("Enter the element to push:");
+                    scanf("%d", &element);
+                    push(s, element);
+                    break;
+                }
+                case 2:
+                {
+                    int data = pop(s);
+                    if (data)
+                    {
+                        printf("The poped element is:%d\n", data);
+                    }
+                    break;
+                }
+                case 3:
+                {
+                    printf("The element at stack top:%c\n", peek(s));
+                    break;
+                }
+                case 4:
+                {
+                    printStack(s);
+                    break;
+                }
+                default:
+                    printf("Invalid input\n");
+                }
+            } while (option != 5);
+            break;
+        }
+        case 2:
+        {
+            char infixExpression[50];
+            printf("Enter the infix expression:");
+            scanf("%s", infixExpression);
+            char *postfixExpression = infixToPostfix(infixExpression);
+            printf("%s", postfixExpression);
+            break;
+        }
+        case 3:
+        {
+            char postfix[50];
+            printf("Enter the postfix expression:");
+            scanf("%s", postfix);
+            printf("Result:%d\n", evaluatePostfix(postfix));
+            break;
+        }
+        case 4:
+        {
+            char exp[50];
+            printf("Enter the expression:");
+            scanf("%s", exp);
+            if (areBracketsBalanced(exp))
+                printf("Balanced \n");
+            else
+                printf("Not Balanced \n");
+            break;
+        }
