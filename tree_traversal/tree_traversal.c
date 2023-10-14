17d20114bca05d734d26d034cdd858422c9bbb20
Pritam <tivrekarpritam7748@gmail.com>
2023-10-14 20:04:39 +0530

    ..

--- tree_traversal/tree_traversal.c
+++ tree_traversal/tree_traversal.c
@@ -10,3 +10,22 @@ void inorderTraversal(node* root){
     printf("%d->", root->data);
     inorderTraversal(root->right);
 }
+
+//  PREORDER TRAVERSAL
+void preorderTraversal(node* root){
+    if(root == NULL)
+    return;
+    printf("%d->", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+//  POSTORDER TRAVERSAL
+void postorderTraversal(node* root){
+    if(root == NULL)
+    return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d->", root->data);
+}
+
