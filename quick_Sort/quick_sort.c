7c73cf62ea37b2e5d0c3f0baea65080113e0df6f
ayush2333 <mca.2333@unigoa.ac.in>
2023-10-14 19:35:19 +0530

    ...

---
+++ quick_Sort/quick_sort.c
@@ -0,0 +1,31 @@
+#include "quick_Sort.h"
+
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high)
+{
+    // Choosing the pivot
+    int pivot = arr[high];
+ 
+    // Index of smaller element and indicates
+    // the right position of pivot found so far
+    int i = (low - 1);
+ 
+    for (int j = low; j <= high - 1; j++) {
+ 
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot) {
+ 
+            // Increment index of smaller element
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
---
+++ quick_Sort/quick_sort.h
@@ -0,0 +1,7 @@
+#ifndef QUICK_SORT
+#define QUICK_SORT
+
+void quickSort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
+void swap(int* a, int* b);
+#endif
